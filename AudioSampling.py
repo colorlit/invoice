340629584d1fa4c24ab53382b85d172cd30c35a6
colorlit <118107184+colorlit@users.noreply.github.com>
2022-11-16 03:53:30 +0900

    Update AudioSampling.py
    add test driver

--- AudioSampling.py
+++ AudioSampling.py
@@ -13,25 +13,58 @@ import os
 
 class AudioSampler(object):
     def __init__(self):
-        #PyAudio Initialization
+        # PyAudio Initialization
         self.pa_format = pyaudio.paFloat32
         self.pa_channels = 1
         self.pa_rate = 44100
         self.pa_chunk = 1024 * 2
         self.p = None
-        self.stream = None
-        self.pa_length = 512
+        self.pa_stream = None
+        #self.pa_length = 512
         self.pa_num_samples = 0
 
-        #Filesystem Initialization
-        self.files = []
+        # Librosa Initialization
+        self.lr_length = 512
+        self.lr_array = None
+        self.lr_chunk = 1024 * 2
+        self.lr_sr = None # note to set 44100 if not initialized by load
+
+        # Filesystem Initialization
+        self.files_list = []
         self.root_dir = os.getcwd()
         self.input_dir = self.root_dir + r'\data\ffmpeg\outputs'
 
+    def pa_stream_open(self):
+        # Intergration WIP
+        self.p = pyaudio.PyAudio()
+        self.pa_stream = self.p.open(format=self.pa_format,
+                                     channels=self.pa_channels,
+                                     rate=self.pa_rate,
+                                     input=True,
+                                     output=False,
+                                     stream_callback=self.callback,
+                                     frames_per_buffer=self.pa_chunk)
+
     def import_file(self):
-        self.files = os.listdir(self.input_dir)
-        #print(self.files)
-        print("{0} of files added.".format(len(self.files)))
+        self.files_list = os.listdir(self.input_dir)
+        for path in os.listdir(self.input_dir):
+            if os.path.isfile(os.path.join(self.input_dir, path)):
+                self.files_list.append(path)
+        #print(self.files_list)
+        print("{0} of files added.".format(len(self.files_list)))
+
+    def lr_load(self):
+        # Test driver
+        file_name = self.input_dir + r'\\' + self.files_list[0]
+        self.lr_array, self.lr_sr = librosa.load(file_name)
+
+    def testd_centroid(self):
+        cent_obj = librosa.feature.spectral_centroid(y=self.lr_array, sr=self.lr_sr, n_fft=self.lr_chunk,
+                                                     hop_length=self.lr_length)[0]
+        print(cent_obj.shape)
+        cent_frames = range(len(cent_obj))
+        cent_t = librosa.frames_to_time(cent_frames, hop_length=self.lr_length)
+        print(len(cent_t))
 
     def start(self):
         pass
@@ -39,11 +72,15 @@ class AudioSampler(object):
     def stop(self):
         print("Process Halted.")
 
+    def callback(self, in_data, frame_count, time_info, flag):
+        # WIP
+        self.lr_array = np.frombuffer(in_data, dtype=np.float32)
+
     def mainloop(self):
         pass
 
 audio = AudioSampler()
-#audio.start()  # open the stream
-#audio.mainloop()  # main operations with librosa
 audio.import_file()
+audio.lr_load()
+audio.testd_centroid()
 audio.stop()
